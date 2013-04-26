cad72c508472bd0727cd86beabe10b8e855b3e6b
Chris Powell <chris.i.powell@gmail.com>
2013-04-26 17:35:24 +0100

    Formbuilder Progress

--- js/starter-formbuilder.js
+++ js/starter-formbuilder.js
@@ -5,6 +5,11 @@ require.config({
      
   },
   shim: {
+       'bootstrap-button':{
+        //These script dependencies should be loaded before loading
+        //backbone.js
+        deps: ['jquery']
+     },
      'bootstrap-tab':{
         //These script dependencies should be loaded before loading
         //backbone.js
@@ -62,12 +67,12 @@ require.config({
   }
 });
 
-require(['jquery', 'jquery-ui.min', 'backbone', "async!http://maps.googleapis.com/maps/api/js?sensor=false", 'bootstrap-tab', 'bootstrap-tooltip', 'bootstrap-dropdown', 'bootstrap-collapse', 'bootstrap-affix', 'bootstrap-modal', 'markerclusterer'], function(){   
+require(['jquery', 'jquery-ui.min', 'backbone', "async!http://maps.googleapis.com/maps/api/js?sensor=false", 'bootstrap-tab', 'bootstrap-tooltip', 'bootstrap-dropdown', 'bootstrap-collapse', 'bootstrap-affix', 'bootstrap-modal','bootstrap-button', 'markerclusterer'], function(){   
     var names = location.pathname.replace(SITE_ROOT, '').replace(/(^\/|\/$)/, '').split('/');    
     var projectName = names[0];
     var formName = names[1];
     var url = location.href;
-    var site_root = location.href.replace(new RegExp(formName + '\/?',''), '').replace(new RegExp(projectName + '\/?',''), '')
+    var site_root = location.href.replace(new RegExp(formName + '\/?',''), '').replace(new RegExp(projectName + '\/?',''), '');
 
     var EpiCollect = {};
 
@@ -87,7 +92,37 @@ require(['jquery', 'jquery-ui.min', 'backbone', "async!http://maps.googleapis.co
     
     var Field = Backbone.Model.extend({
         name : '',
-        label : ''
+        label : '',
+        type : '',
+        getFormBuilderType : function(){
+            var type = ''
+            
+            if ( this.type === 'input' )
+            {
+                if(this.isInt || this.isDouble)
+                {
+                    type = 'numeric';
+                }
+                else if ( this.date || this.setDate)
+                {
+                    type =  'date';
+                }
+                else if ( this.time || this.setTime)
+                {
+                    type =  'time';
+                }
+                else
+                {
+                    type =  'text';
+                }
+            }
+            else 
+            {
+                type =  this.type;
+            }
+            
+            return 'ecplus-' + type + '-element';
+        }
     });
     
     var FieldCollection = Backbone.Collection.extend({
@@ -122,7 +157,8 @@ require(['jquery', 'jquery-ui.min', 'backbone', "async!http://maps.googleapis.co
     });
      
     var project = new Project();
-
+    
+    
    $('#props div').tooltip();
    
    function setupDraggable()
@@ -164,21 +200,33 @@ require(['jquery', 'jquery-ui.min', 'backbone', "async!http://maps.googleapis.co
     
     
     var FormList = Backbone.View.extend({
+        
         initialize : function()
         {
             this.render();
+            
+            this.listenTo(this.model, 'reset', this.addAll);
+            this.listenTo(this.model, 'add', this.addForm);
+            
         },
         render : function()
         {
-            this.$('.form').remove();
-
-            _.each(this.model, function (form){
-               if(form.main){
-                   this.$el.append('<span id="' + form.name + '" class="form">' + form.name + '</span>'); 
-               }
-            }, this);
+            this.addAll();
             
             if(this.model.length > 0) this.$el.trigger({ type : 'first_render' });
+        },
+        addAll :function(){
+            this.$('.form').remove();
+
+            this.model.each(function (f){
+                this.addForm(f);
+            }, this);
+        },
+        addForm : function(form){
+    
+             if(form.get('main')){
+                this.$el.append('<span id="' + form.get('name') + '" class="form">' + form.get('name') + '</span>'); 
+            }
         },
         select : function(name)
         {
@@ -191,61 +239,107 @@ require(['jquery', 'jquery-ui.min', 'backbone', "async!http://maps.googleapis.co
         events : {
            'change #create-form-input' : 'validateFormNameInput',
            'click #add_form' : 'addFormDialogOK',
-           'first_render #formList' : 'selectFirstForm'
+           'click #add_key' : 'addKeyDialogOK',
+           'first_render #formList' : 'selectFirstForm',
+           'click #input_key' : 'showKeyDetails',
+           'click #gen_key' : 'hideKeyDetails'
         },
         initialize : function(){
-            var $el = this.$el;
             var ctx = this;
 
             this.formList = this.options.formList;
 
-            this.model.fetch({ url : SITE_ROOT + '/' + projectName + '.json',
+            project.fetch({ url : SITE_ROOT + '/' + projectName + '.json',
                 success : function(){ 
-                    if (ctx.model.get('tables').length === 0)
+                    if (project.get('tables').length === 0)
                     {
-                        
-                       $el.append(builder.options.dialog_template({ 
-                            id: 'create-form-dialog', 
-                            title : 'Name your first form', 
-                            message : '<p>Please choose a name for your first form</p><input type="text" name="form_name" id="create-form-input" /><div id="create-form-input-validation"></div>',
-                            preventClose : true,
-                            buttons : [{
-                                label : 'Add Form',
-                                id : 'add_form',
-                                type : 'btn-success'
-                            }]
-                        }));
-                        
-                        $('#create-form-dialog').modal({ backdrop : 'static' });
+                       ctx.promptForFormName();
                     }
                     else
                     {
-                        ctx.formList.model = ctx.model.get('tables');
+                        ctx.formList.model.reset(project.get('tables'));
                     } 
                     ctx.formList.render();
                 } 
             });
+            
+            setupDraggable();
+            
+            this.listenTo(this.model, 'reset', this.redraw);
+            this.listenTo(this.model, 'add', this.addField);
+            
         },
         addFormDialogOK : function()
         {
+            
+            var name = this.$('#create-form-dialog input').val();
             if(this.validateFormNameInput())
             {
-                var name = this.$('#create-form-dialog input').val();
-                if(this.validateFormNameInput)
-                {
-                   var frm = { 
-                       name : name,
-                       num : this.model.tables.length + 1,
-                       key : null
-                   };
+               var frm = { 
+                   name : name,
+                   num : this.formList.model.length + 1,
+                   key : null,
+                   fields : [],
+                   main : true
+               };
 
-                   this.model.tables.push(frm);
-                   $('#create-form-dialog').modal('hide');
-                   this.formList.model = this.model.get('tables');
-                   this.formList.render();
-                   this.selectForm(name);
-                }
+               this.formList.model.add(frm);
+
+                $('#create-form-dialog').modal('hide');
+
+               this.formList.render();
+               this.selectForm(name);
             }
+            
+        },
+        addField : function(fld){
+      
+            var ctrl = this.$('#source .' + fld.getFormBuilderType()).clone();
+            ctrl.attr('id', fld.name)
+            $('.title', ctrl).text(fld.label);
+      
+            this.$('#destination').append(ctrl);
+            
+        },
+        addKeyDialogOK : function()
+        {
+
+            if(this.$('#input_key').hasClass('active'))
+            {
+                this.addInputKey();
+            }
+            else
+            {
+                this.addGenKey();
+            }
+            $('#create-key-dialog').modal('hide');
+        },
+        addGenKey : function()
+        {
+            var fld = new Field();
+            fld.genkey = true;
+            fld.isKey = true;
+            fld.type = 'input';
+            fld.label = 'Entry Key';
+            fld.name = 'entry_key';
+            
+            this.model.add(fld);
+            
+        },
+        addInputKey : function()
+        {
+            var fld = new Field();
+            var props = this.$('#key_details').serializeArray();
+            var vals = _.object(_.pluck(props, 'name'), _.pluck(props, 'value'));
+            console.debug(vals);
+            
+            fld.genkey = true;
+            fld.isKey = true;
+            fld.type = vals['key_type']=== 'barcode' ? 'barcode' : 'input';
+            fld.label = vals['key_label'];
+            fld.name = vals['key_name'];
+            fld.isInt = vals['key_type'] === 'numeric'
+            this.model.add(fld);
         },
         getFormBuiderType : function(ctrl)
         {
@@ -278,11 +372,53 @@ require(['jquery', 'jquery-ui.min', 'backbone', "async!http://maps.googleapis.co
             }
             
         },
+        promptForFormName : function()
+        {
+             this.$el.append(this.options.dialog_template({ 
+                id: 'create-form-dialog', 
+                title : 'Name your first form', 
+                message : '<p>Please choose a name for your first form</p><input type="text" name="form_name" id="create-form-input" /><div id="create-form-input-validation"></div>',
+                preventClose : true,
+                buttons : [{
+                    label : 'Add Form',
+                    id : 'add_form',
+                    type : 'btn-success'
+                }]
+            }));
+
+            $('#create-form-dialog').modal({ backdrop : 'static' });
+        },
+        promptForKey : function()
+        {
+             this.$el.append(builder.options.dialog_template({ 
+                id: 'create-key-dialog', 
+                title : 'Name your first form', 
+                message : '<p>Every entry should have a unique key, this key can be generated or typed in by the user. If you are unsure of which to use we recommend using a generated key. What kind of key should this form have?</p><div><div class="btn-group" data-toggle="buttons-radio">' +
+                            '<button id="gen_key" type="button" class="btn" data-toggle="tooltip" data-placement="top" title="Selecting this option will mean a key is generated for each entry that is designed to be unique to a handset.">A Generated Key</button>' +
+                            '<button id="input_key" type="button" class="btn" data-toggle="tooltip" data-placement="top" title="Selecting this option means your user will be asked to enter a key, this can lead to the same key being entered or different phones. In this case the second user to send their data to the server will be informed when they try and synchronise their data.">An Inputted Key</button>' +
+                            '</div></div><form id="key_details"><fieldset>'+
+                            '<label for="key_name">Key Name</label><input type="text" name="key_name" id="key_name" />'+
+                            '<label for="key_label">Key Label</label><input type="text" name="key_label" id="key_label" />'+
+                            '<label for="key_type">Key Type</label><select name="key_type" id="key_type" ><option value=\"text\">Text Field</option><option value=\"numeric\">Integer Field</option><option value=\"barcode\">Barcode Field</option></select>'+
+                            '</fieldset></form><div id="create-form-input-validation"></div>',
+                preventClose : true,
+                buttons : [{
+                    label : 'Add Key',
+                    id : 'add_key',
+                    type : 'btn-success'
+                }]
+            }));
+            this.hideKeyDetails();
+            $('#create-key-dialog').modal({ backdrop : 'static' });
+        },
+        redraw : function()
+        {
+            $('#destination').empty();
+        },
         renderForm: function(name)
         {
-            var form = _(this.model.get('tables')).findWhere({ name : name })
             
-            console.debug( form );
+            var form = this.formList.model.findWhere({ name : name });
             
             _.each(form.fields, function(fld)
             {
@@ -304,19 +440,28 @@ require(['jquery', 'jquery-ui.min', 'backbone', "async!http://maps.googleapis.co
             var optionTemplate = _.template($('#option-template').html());
             
             _(ctrl.options).each(function(opt){
-                console.debug(opt);
                ctl.append(optionTemplate(opt)); 
             });
             
+        },
+        showKeyDetails : function()
+        {
+           this.$('#key_details').show();  
+        },
+        hideKeyDetails : function()
+        {
+           this.$('#key_details').hide();  
         },
         selectForm : function(name) 
         {
             this.formList.select(name);
             this.renderForm(name);
+            
+            this.promptForKey();
         },
         selectFirstForm : function()
         {
-            var name = _.findWhere(this.model.get('tables'), { num : 1 }).name;
+            var name = this.formList.model.findWhere({ num : 1 }).get('name');
             this.selectForm(name);
         },
         validateFormNameInput : function()
@@ -344,21 +489,18 @@ require(['jquery', 'jquery-ui.min', 'backbone', "async!http://maps.googleapis.co
         
        
     });
-    
-    
+
     var builder = new FormBuilder({
         el : $('#main'),
         formList : new FormList({ 
             el : $('#formList'),
-            model : project.tables
+            model : new FormCollection()
         }),
         alert_template : _.template($('#alert-template').html()),
         dialog_template :  _.template($('#dialog-template').html()),
-        model : project
+        model : new FieldCollection()
     });
-   
-   setupDraggable();
-   
+
 });
 
 
